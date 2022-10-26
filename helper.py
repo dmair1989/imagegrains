80880260fc2e8f180a5e27cb4a77519a7a45da2a
dmair1989 <david.mair1@gmx.at>
2022-10-26 18:06:22 +0200

    start of cellpose helper class

---
+++ helper.py
@@ -0,0 +1,92 @@
+import numpy as np
+from cellpose import io
+from tqdm import tqdm
+from glob import glob
+from natsort import natsorted
+
+class cellpose_helper:
+
+    def predict_folder(INP_DIR,model,image_format='jpg',channels=[0,0],diameter=None,min_size=15,rescale=None,TAR_DIR='',return_results=False,save_masks=True):
+        """ Do predictions on all images with `image_format` extension in a folder. 
+        If `return_results` is `True` respective lists of 1D arrays for predicted *masks*, *flows* and *styles* 
+        from `CellposeModel.eval()` are returned (see https://cellpose.readthedocs.io/en/latest/api.html#id5).
+
+        Parameters:
+        ------------
+        IND_DIR: Directory path (str)
+        model: Trained model from 'models.CellposeModel' class. 
+            Use either `models.CellposeModel(model_type='')` for built-in cellpose models or 
+            `models.CellposeModel(pretrained:model='') for custom models.
+            See https://cellpose.readthedocs.io/en/latest/models.html for more details
+        image_fromat (str(optional, default 'jpg'))
+        return_results (bool(optional, default False))
+        TAR_DIR (str(optional, default '')) - output directory
+        save_masks (bool(optional, default True)) - flag for saving predicted mask as `.tif` files in `TAR_DIR`
+
+        Parameters that can be handed down to`CellposeModel.eval()`, 
+        see https://cellpose.readthedocs.io/en/latest/api.html#id5 :
+
+        channels (list (optional, default [0,0]))
+        diameter (float (optional, default None))
+        rescale (float (optional, default None))
+        min_size (int (optional, default 15))   
+        
+        
+        Returns
+        ------------
+        masks (optional; list of 2D arrays)
+            labelled image, where 0=no masks; 1,2,…=mask labels
+
+        flows (optional; list of lists 2D arrays) 
+            flows[k][0] = XY flow in HSV 0-255 flows[k][1] = XY flows at each pixel 
+            flows[k][2] = cell probability (if > cellprob_threshold, pixel used for dynamics) 
+            flows[k][3] = final pixel locations after Euler integration
+
+        styles (optional; list of 1D arrays of length 64) 
+            style vector summarizing each image, also used to estimate size of objects in image
+
+        """
+        try:
+            Z = sorted(glob(INP_DIR+'/*.'+image_format))
+            print('Predicting masks for files in',INP_DIR,'...')
+            if return_results == True:
+                mask_l,flow_l,styles_l,ID_l = [],[],[],[]
+            for j in tqdm(range(len(Z)),unit='image',colour='CYAN'):
+                img= io.imread(Z[j])
+                masks, flows, styles = model.eval(img, diameter=diameter,rescale=rescale,min_size=min_size,channels=channels,min_size=-1) 
+                ID = Z[0].split('\\')[len(Z[0].split('\\'))-1].split('.')[0]
+                if save_masks == True:
+                    io.imsave(TAR_DIR+ID+'_pred.tif',masks)
+                if return_results == True:
+                    mask_l.append(masks),flow_l.append(flows),styles_l.append(styles),ID_l.append(ID)
+                break
+            print('Sucessfully created predictions for',j+1,'images.')
+        except KeyboardInterrupt:
+            print('Aborted.')
+        if return_results == True:
+            return(mask_l,flow_l,styles_l,ID_l)
+    
+    def models_from_zoo(MOD_DIR):
+        model_list = natsorted(glob(MOD_DIR+'*.*'))
+        return(model_list)
+
+    def images_from_split_data(IMG_DIR):
+        img_list=[]
+        #try if train, test directory exist
+            #get files from there
+        #else: get files from IMG_DIR
+        #filter for png, tif, jpg
+        #remove imgs with _mask, _masks, _flows strings
+        return(img_list)
+    
+    def labels_from_split_data(LBL_DIR):
+        lbl_list = []
+        #try if train, test directory exist
+            #get files from there
+        #else: get files from IMG_DIR
+        #filter for png, tif, jpg
+        #remove entries that have not _mask, _masks strings
+        return(lbl_list)
+
+
+print('ok')
