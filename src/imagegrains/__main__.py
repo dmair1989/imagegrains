b5423cf6198a436f4a34af1496340b0f495eb6f9
dmair1989 <david.mair1@gmx.at>
2023-03-16 19:39:50 +0100

    add uncertainty step

--- src/imagegrains/__main__.py
+++ src/imagegrains/__main__.py
@@ -25,12 +25,24 @@ def main():
     gs_args=parser.add_argument_group('Grain size estimation')
     gs_args.add_argument('--min_grain_size', type=float, default=None, help='Minimum grain size in pixels to consider for grain size estimation (default: None); grains with a fitted ellipse smaller than this size will be ignored.')
     gs_args.add_argument('--edge_filter', type=float, default=None, help = 'Edge filter to remove grains close to the image boundary (default: None).')
-    gs_args.add_argument('--fit', type=str, default=None, help='Additional approximation for grains (default: None); options are convex hull (convh) or outline (outl).')
-    gs_args.add_argument('--grid_resample', type=int, default=None, help = 'Resample images with a grid with a given resolution in pixel (default: None). Equivalent ot a digital Wolman grid.')
-    gs_args.add_argument('--random_resample', type=int, default=None, help = 'Resample image with a random number of points (default: None).')
+    gs_args.add_argument('--fit', type=str, default=None, help='Additional approximation for grains (default: None); options are convex hull (convex_hull) or outline (mask_outline).')
+    gs_args.add_argument('--grid_resample', default=None, help = 'Resample images with a grid with a given resolution in pixel (default: None). Equivalent ot a digital Wolman grid.')
+    gs_args.add_argument('--random_resample', default=None, help = 'Resample image with a random number of points (default: None).')
     gs_args.add_argument('--resolution', default=None, help = 'Image resolution to scale grain sizes to in mm/px (default: None). If a value is provided, the grain sizes will be scaled to the given resolution. Alternatively, can provided as path to a csv file with image_specific resolutions (see template). For estimating the image resolution from camera parameters see the preprocessing notebook.')
 
-    args = parser.parse_args()
+    gsd_args=parser.add_argument_group('GSD analysis')
+    gsd_args.add_argument('--unc_method', type = str, default = 'bootstrapping', help = 'Method to estimate uncertainty of grain size distribution (default: bootstrap). Options are bootstraping (bootstrapping), simpple Monte Carlo (MC), or advanced Monte Carlo for SfM data (MC_SfM_OM or MC_SfM_SI).')
+    gsd_args.add_argument('--n', type = int, default = 1000, help = 'Number of iterations for uncertainty estimation (default: 10000).')
+    gsd_args.add_argument('--scale_err', default=0.1, help='Scale error for MC uncertainty estimation in fractions (default: 0.1).')
+    gsd_args.add_argument('--length_err', default=0.1, help='Length error for MC uncertainty estimation in pixel or mm (default: 1); whether it is interpreted as py or mm value depends resolution was provided or not.')
+    gsd_args.add_argument('--SfM_file', default = None, help = 'Path to SfM uncertainty file (default: None). See template for details.')
+    gsd_args.add_argument('--summary', type = bool, default = True, help = 'Save key percentile summary for all images (default: True).')
+
+    try:
+        args = parser.parse_args()
+    except:
+        parser.print_help()
+        exit()
 
     mute = False if args.mute_output else True
 
@@ -85,6 +97,14 @@ def main():
         if resampled == True:
             scaling_step(resample_path,args.resolution,gsd_str='resampled_grains',mute=True,TAR_DIR=TAR_DIR)
 
+    #gsd analysis
+    print ('>> ImageGrains: Calculating grain size distributions and uncertainties for ',args.img_dir)
+    gsd_step(args.img_dir,args,mute=mute,TAR_DIR=TAR_DIR)
+    if resampled == True:
+        print('>> Calculating grain size distributions and uncertainties for ',resample_path)
+        gsd_step(resample_path,args,mute=mute,TAR_DIR=TAR_DIR)
+
+
 def segmentation_step(args,mute=False,TAR_DIR=''):
     if args.gpu == True:
         if torch.cuda.is_available() == True and mute == False:
@@ -122,7 +142,7 @@ def segmentation_step(args,mute=False,TAR_DIR=''):
 def resampling_step(args,filters,mute=False,TAR_DIR=''):    
     if args.grid_resample:
         method = 'wolman'
-        grid_size= args.grid_resample
+        grid_size= int(args.grid_resample)
         if mute == False:
             print('>> Resampling grains with a grid with a resolution of ',args.grid_resample,' pixels.')
 
@@ -193,6 +213,67 @@ def scaling_step(img_dir,resolution,mute=False,gsd_str='_grains',TAR_DIR=''):
         if mute == False:
             print('>> Rescaled grains image-specific resolutions from',resolution,'.')
         return
+
+def gsd_step(PATH,args,mute=False,TAR_DIR=''):
+    grains= data_loader.load_grain_set(PATH,gsd_str='grains_re_scaled')
+    scaled = True
+    if not grains:
+        grains = data_loader.load_grain_set(PATH,gsd_str='grains')
+        scaled = False
+        if mute == False:
+            print('No scaled grains found. Loading unscaled grains.')
+    if scaled == True:
+        columns = ['ell: a-axis (mm)','ell: b-axis (mm)']
+        if args.fit == 'convex_hull':
+            columns += ['convex hull: a axis (mm)','convex hull: b axis (mm)']
+        if args.fit == 'mask_outline':
+            columns += ['mask outline: a axis (mm)','mask outline: b axis (mm)']
+        method = args.unc_method
+        if method == 'MC_SfM':
+            sfm_err = gsd_uncertainty.compile_sfm_error(args.SfM_file)
+            if 'OM' in args.SfM_file:
+                sfm_type = 'OM'
+            else:
+                sfm_type = 'SI'
+
+    else:
+        columns = ['ell: a-axis (px)','ell: b-axis (px)']
+        if args.fit == 'convex_hull':
+            columns += ['convex hull: a axis (px)','convex hull: b axis (px)']
+        if args.fit == 'mask_outline':
+            columns += ['mask outline: a axis (px)','mask outline: b axis (px)']
+        method = 'bootstrapping'
+    ids = [str(Path(x).stem) for x in grains]
+    df_list = []
+    for i,column in enumerate(columns):
+        if mute == False:
+            print(column)
+
+        column_unc = gsd_uncertainty.dataset_uncertainty(gsds=grains,gsd_id=ids,return_results=True,
+                                                save_results=False,method=method,column_name=column,
+                                                num_it=args.n,scale_err=args.scale_err,length_err=args.length_err,
+                                                sfm_error=sfm_err,sfm_type=sfm_type,mute=True)        
+        for j,idi in enumerate(ids):
+            if i == 0:
+                df = pd.DataFrame({f'{column}_perc_lower_CI':column_unc[idi][2],
+                                f'{column}_perc_median':column_unc[idi][0],
+                                f'{column}_perc_upper_CI':column_unc[idi][1],
+                                f'{column}_perc_value':column_unc[idi][3]})
+                df_list.append(df)
+            else:
+                df_list[j]=pd.concat([df_list[j],pd.DataFrame({f'{column}_perc_lower_CI':column_unc[idi][2],
+                                f'{column}_perc_median':column_unc[idi][0],
+                                f'{column}_perc_upper_CI':column_unc[idi][1],
+                                f'{column}_perc_value':column_unc[idi][3]})],axis=1)
+    if TAR_DIR != '':
+        out_dir = TAR_DIR
+    else:
+        out_dir = PATH
+    for idi, dfi in zip(ids,df_list):
+        idi = idi.replace('grains','')
+        dfi.to_csv(f'{out_dir}/{idi}_{method}_full_uncertainty.csv')
+    #summry stats will go here
+    return 
 
 if __name__ == '__main__':
     main()
